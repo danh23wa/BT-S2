ffb4c2f647a95386cd14b596a31b4cbdbadc56ac
Huynh Tuan <huynhtuan9251@gmail.com>
2025-09-22 09:22:34 +1400

    [Feature] Update btn

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -46,7 +46,7 @@ const Login: React.FC = () => {
         onClick={handleLogin}
         className="bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition"
       >
-        Đăng nhập
+        đăng nhập
       </button>
     </div>
   );
